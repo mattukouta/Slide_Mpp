29c4ca210c3744f88ffa040d553d645040f59509
Kouta Takamatsu <bb1016181@gmail.com>
2020-07-23 01:15:53 +0900

    ios用のdispatcher部分の追加

--- SharedCode/src/iosMain/kotlin/actual.kt
+++ SharedCode/src/iosMain/kotlin/actual.kt
@@ -1,3 +1,20 @@
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Runnable
 import platform.UIKit.UIDevice
+import platform.darwin.dispatch_async
+import platform.darwin.dispatch_get_main_queue
+import platform.darwin.dispatch_queue_t
+import kotlin.coroutines.CoroutineContext
 
 actual fun platformName(): String = UIDevice.currentDevice.systemName() + UIDevice.currentDevice.systemVersion
+
+internal actual val coroutineDispatcher: CoroutineDispatcher = NsQueueDispatcher(dispatch_get_main_queue())
+
+internal class NsQueueDispatcher(private val dispatchQueue: dispatch_queue_t): CoroutineDispatcher() {
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        dispatch_async(dispatchQueue) {
+            block.run()
+        }
+    }
+
+}
