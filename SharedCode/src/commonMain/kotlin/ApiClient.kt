bfc34d9e58c2c5066322852f5d3f6c52da6be7f5
Kouta Takamatsu <bb1016181@gmail.com>
2020-07-24 20:56:41 +0900

    twitter apiの取得ロジック実装

--- SharedCode/src/commonMain/kotlin/ApiClient.kt
+++ SharedCode/src/commonMain/kotlin/ApiClient.kt
@@ -1,25 +1,31 @@
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
-import io.ktor.client.request.url
-import io.ktor.client.statement.HttpResponse
-import io.ktor.client.statement.readText
-import io.ktor.http.URLProtocol
+import io.ktor.client.request.header
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 
 internal expect val coroutineDispatcher: CoroutineDispatcher
 class ApiClient {
     private val httpClient = HttpClient()
 
+    @UnstableDefault
     fun getTweets(successCallback: (Tweets) -> Unit, errorCallback: (Exception) -> Unit) {
         GlobalScope.launch(coroutineDispatcher) {
             try {
-                val url = "http://www.json-generator.com/api/json/get/cfUkWOpFiW?indent=2"
-                val result = httpClient.get<HttpResponse>{url(url)}.readText()
-
-                val tweets = Json.parse(Tweets.serializer(), result)
+                val url = "https://api.twitter.com/1.1/search/tweets.json?q=%23ca_fun_lt&count=2&result_type=recent&include_entities=false"
+                val result = httpClient.get<String>(url){
+                    header("Authorization","Bearer AAAAAAAAAAAAAAAAAAAAANpkGAEAAAAANOXZxNK680LbU1vtoWaOGo%2BJRmA%3D221IaVZ4TkrzW87H7QxcSvoRW4WAFFMkxAUhcuBAKyjTjCoum3")
+                }
+                val tweets = Json(
+                    JsonConfiguration(
+                        strictMode = false,
+                        useArrayPolymorphism = true
+                    )
+                ).parse(Tweets.serializer(), result)
 
                 successCallback(tweets)
             } catch (e: Exception) {
--- SharedCode/src/commonMain/kotlin/Tweets.kt
+++ SharedCode/src/commonMain/kotlin/Tweets.kt
@@ -1,4 +1,46 @@
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Tweets(val productId: String, val purchaseToken: String)
+data class Tweets(val statuses: List<Statuses>?, val search_metadata: SearchMetadata?) {
+    @Serializable
+    data class Statuses(
+        val created_at: String?,
+        val id_str: String?,
+        val text: String?,
+        val source: String?,
+        val user: User?
+    ) {
+        @Serializable
+        data class User(
+            val id_str: String?,
+            val name: String?,
+            val screen_name: String?,
+            val description: String?,
+            val url: String?,
+            val profile_background_color: String?,
+            val profile_background_image_url: String?,
+            val profile_background_image_url_https: String?,
+            val profile_background_tile: Boolean?,
+            val profile_image_url: String?,
+            val profile_image_url_https: String?,
+            val profile_banner_url: String?,
+            val profile_link_color: String?,
+            val profile_sidebar_border_color: String?,
+            val profile_sidebar_fill_color: String?,
+            val profile_text_color: String?
+        )
+    }
+
+    @Serializable
+    data class SearchMetadata(
+        val completed_in: Double?,
+        val max_id: Long?,
+        val max_id_str: String?,
+        val next_results: String?,
+        val query: String?,
+        val refresh_url: String?,
+        val count: Int?,
+        val since_id: Int?,
+        val since_id_str: String?
+    )
+}
