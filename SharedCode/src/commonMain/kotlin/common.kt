bad64b7a579974343d4cee0c0088a82fb654807a
Kouta Takamatsu <bb1016181@gmail.com>
2020-07-25 00:57:10 +0900

    表示するスライド用の一部テキストを追加

--- SharedCode/src/commonMain/kotlin/common.kt
+++ SharedCode/src/commonMain/kotlin/common.kt
@@ -1,3 +1,7 @@
+import SlideInfo.Companion.slideInfo
+
 expect fun platformName(): String
 
 fun createApplicationScreenMessage(): String =  "Kotlin Rocks on ${platformName()}"
+
+fun getSlideInfo(): List<Slide> = slideInfo
