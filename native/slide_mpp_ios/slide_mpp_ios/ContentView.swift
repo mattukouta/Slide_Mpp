0b24ae43188475653b1bd369b396acd76fa9bf01
Kouta Takamatsu <bb1016181@gmail.com>
2020-08-01 05:12:33 +0900

    コンテンツ用のVIew作成

--- native/slide_mpp_ios/slide_mpp_ios/ContentView.swift
+++ native/slide_mpp_ios/slide_mpp_ios/ContentView.swift
@@ -11,8 +11,52 @@ import SwiftUI
 struct ContentView: View {
     var text: String
     var body: some View {
-        ZStack(alignment:.topLeading) {
-            Text("Hello, \(text)")
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Hello, \(self.text)")
+                        .font(.largeTitle)
+                    Rectangle()
+                        .fill(Color.red)
+                        .frame(width: geometry.size.width - 100, height: 1)
+                    
+                    ForEach(1..<3) { localIndex in
+                        HStack {
+                            Image("ic-send")
+                                .resizable()
+                                .frame(width: 30, height: 30, alignment: .leading)
+                            
+                            Text("Hello, \(self.text)")
+                                .font(.title)
+                        }
+                        
+                        ForEach(1..<3) { localIndex in
+                            HStack {
+                                Image("ic-arrow")
+                                    .resizable()
+                                    .frame(width: 20, height: 20, alignment: .leading)
+                                
+                                Text("Hello, \(self.text)")
+                                .font(.headline)
+                            }
+                            
+                            ForEach(1..<3) { localIndex in
+                                HStack {
+                                    Image("ic-next")
+                                        .resizable()
+                                        .frame(width: 20, height: 20, alignment: .leading)
+                                    
+                                    Text("Hello, \(self.text)")
+                                    .font(.body)
+                                }
+                            }.offset(x: 20, y: 0)
+                        }.offset(x: 20, y: 0)
+                    }.offset(x: 10, y: 0)
+                    
+                    Spacer()
+                    
+                }
+            }
         }
     }
 }
