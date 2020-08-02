695868be30341985e29f9e16d42342648cf6633f
Kouta Takamatsu <bb1016181@gmail.com>
2020-08-02 18:19:26 +0900

    CoverViewでslideInfoを受け取り

--- native/slide_mpp_ios/slide_mpp_ios/CoverView.swift
+++ native/slide_mpp_ios/slide_mpp_ios/CoverView.swift
@@ -7,20 +7,21 @@
 //
 
 import SwiftUI
+import SharedCode
 
 struct CoverView: View {
-    var title: String
+    var slideInfo: Slide
     var body: some View {
         GeometryReader { geometry in
-            Text("Kotlinで始めるクロスプラットフォーム開発")
+            Text(self.slideInfo.component2())
                 .font(.system(size: FontManager().getCoverTitleFontSize()))
                 .frame(width: geometry.size.width)
         }
     }
 }
 
-struct CoverView_Previews: PreviewProvider {
-    static var previews: some View {
-        CoverView(title: "title")
-    }
-}
+//struct CoverView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CoverView(slideInfo: "title")
+//    }
+//}
