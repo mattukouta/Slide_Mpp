d9766d32b5648880944ebd257541cecfc26592e2
Kouta Takamatsu <bb1016181@gmail.com>
2020-08-06 10:43:38 +0900

    背景画像追加

--- native/slide_mpp_ios/slide_mpp_ios/ContentView.swift
+++ native/slide_mpp_ios/slide_mpp_ios/ContentView.swift
@@ -17,69 +17,78 @@ struct ContentView: View {
     var body: some View {
         GeometryReader { parentGeometry in
             ScrollView(.vertical) {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text(self.slideInfo.component2())
-                        .font(.system(size: self.fontManager.getContentTitleFontSize()))
-                    Rectangle()
-                        .fill(Color.red)
-                        .frame(width: parentGeometry.size.width, height: 1)
+                ZStack {
+                    Image("slide_background")
+                    .resizable()
+                    .frame(width: parentGeometry.size.width, height: parentGeometry.size.height)
+                    .rotationEffect(.degrees(180))
                     
-                    HStack {
-                        VStack(alignment: .leading, spacing: 0) {
-                            ForEach([Slide.SubContent](self.slideInfo.component3() ?? []), id: \.self) { slideContentInfoItem in
-                                VStack(spacing: 0) {
-                                    HStack {
-                                        Image("ic-send")
-                                            .resizable()
-                                            .frame(width: self.imageManager.getSendIconSize(), height: self.imageManager.getSendIconSize(), alignment: .leading)
-                                        
-                                        Text(verbatim: "\(slideContentInfoItem.component1().allKeys.first ?? "")")
-                                            .font(.system(size: self.fontManager.getContentSubTitleFontSize()))
-                                        
-                                        Spacer()
-                                    }
-                                    
-                                    ForEach((slideContentInfoItem.component1().allValues.first ?? "") as? [Slide.SubContentSubSubContent] ?? [], id: \.self) { slideSubContentInfoItem in
-                                        VStack(spacing: 0) {
-                                            HStack {
-                                                Image("ic-arrow")
-                                                    .resizable()
-                                                    .frame(width: self.imageManager.getArrowIconSize(), height: self.imageManager.getArrowIconSize(), alignment: .leading)
-
-                                                Text(verbatim: "\(slideSubContentInfoItem.component1().allKeys.first ?? "")")
-                                                    .font(.system(size: self.fontManager.getContentSubSubTitleFontSize()))
-                                                
-                                                Spacer()
-                                            }
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text(self.slideInfo.component2())
+                            .font(.system(size: self.fontManager.getContentTitleFontSize()))
+                        Rectangle()
+                            .fill(Color.red)
+                            .frame(width: parentGeometry.size.width, height: 1)
+                        
+                        HStack {
+                            VStack(alignment: .leading, spacing: 0) {
+                                ForEach([Slide.SubContent](self.slideInfo.component3() ?? []), id: \.self) { slideContentInfoItem in
+                                    VStack(spacing: 0) {
+                                        HStack {
+                                            Image("ic-send")
+                                                .resizable()
+                                                .frame(width: self.imageManager.getSendIconSize(), height: self.imageManager.getSendIconSize(), alignment: .leading)
                                             
-                                            ForEach((slideSubContentInfoItem.component1().allValues.first ?? "") as? [String] ?? [], id: \.self) { slideSubContentItem in
+                                            Text(verbatim: "\(slideContentInfoItem.component1().allKeys.first ?? "")")
+                                                .font(.system(size: self.fontManager.getContentSubTitleFontSize()))
+                                            
+                                            Spacer()
+                                        }
+                                        
+                                        ForEach((slideContentInfoItem.component1().allValues.first ?? "") as? [Slide.SubContentSubSubContent] ?? [], id: \.self) { slideSubContentInfoItem in
+                                            VStack(spacing: 0) {
                                                 HStack {
-                                                    Image("ic-next")
+                                                    Image("ic-arrow")
                                                         .resizable()
-                                                        .frame(width: self.imageManager.getNextIconSize(), height: self.imageManager.getNextIconSize(), alignment: .leading)
+                                                        .frame(width: self.imageManager.getArrowIconSize(), height: self.imageManager.getArrowIconSize(), alignment: .leading)
 
-                                                    Text(verbatim: "\(slideSubContentItem)")
-                                                    .font(.system(size: self.fontManager.getContentSubSubTitleListFontSize()))
+                                                    Text(verbatim: "\(slideSubContentInfoItem.component1().allKeys.first ?? "")")
+                                                        .font(.system(size: self.fontManager.getContentSubSubTitleFontSize()))
                                                     
                                                     Spacer()
                                                 }
-                                            }.offset(x: 20, y: 0)
-                                        }
-                                    }.offset(x: 10, y: 0)
+                                                
+                                                ForEach((slideSubContentInfoItem.component1().allValues.first ?? "") as? [String] ?? [], id: \.self) { slideSubContentItem in
+                                                    HStack {
+                                                        Image("ic-next")
+                                                            .resizable()
+                                                            .frame(width: self.imageManager.getNextIconSize(), height: self.imageManager.getNextIconSize(), alignment: .leading)
+
+                                                        Text(verbatim: "\(slideSubContentItem)")
+                                                        .font(.system(size: self.fontManager.getContentSubSubTitleListFontSize()))
+                                                        
+                                                        Spacer()
+                                                    }
+                                                }.offset(x: 20, y: 0)
+                                            }
+                                        }.offset(x: 10, y: 0)
+                                    }
                                 }
-                            }
-                        }.offset(x: 5, y: 0)
-                        
+                                Spacer()
+                            }.offset(x: 5, y: 0)
+                            
+                            Spacer()
+                            
+                            Image(self.imageManager.getImageName(originalImageName: self.slideInfo.component4() ?? ""))
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: self.imageManager.getContentImageWidth(), maxHeight: self.imageManager.getContentImageHeight())
+                            
+                            Spacer()
+                        }
                         Spacer()
-                        
-                        Image(self.imageManager.getImageName(originalImageName: self.slideInfo.component4() ?? ""))
-                            .resizable()
-                            .scaledToFit()
-                            .frame(maxWidth: self.imageManager.getContentImageWidth(), maxHeight: self.imageManager.getContentImageHeight())
-                        
-                        Spacer()
-                    }
-                }.offset(x: 5, y: 0)
+                    }.offset(x: 5, y: 0)
+                }
             }
         }
     }
